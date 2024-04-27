2ad4fca74e12c9c6e12aab861454f067990a3e22
mariana <marianbodario@gmail.com>
2024-04-26 22:36:01 -0300

    Instalação dos campos e complementos do cep e formatação

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,26 @@
 function ConsultaCep() {
+    
+    let campo = document.querySelector("#cep")
     let requisicao = new XMLHttpRequest();
-    let url = "https://viacep.com.br/ws/"++"/json/";
+    let url = "https://viacep.com.br/ws/"+ campo.value +"/json/";
     requisicao.open("GET", url);
     requisicao.send();
     requisicao.onload = function () {
-        alert(JSON.parse(requisicao.response).logradouro);
+        let objectCEP = JSON.parse(requisicao.response)
+        let campoEndereco = document.querySelector("#logradouro")
+        campoEndereco.value = objectCEP.logradouro
+
+        let campoComplemento = document.querySelector("#Complemento")
+        campoComplemento.value = objectCEP.complemento
+
+        let campoBairro = document.querySelector("#Bairro")
+        campoBairro.value = objectCEP.bairro
+
+        let campoLocalidade = document.querySelector("#Localidade")
+        campoLocalidade.value = objectCEP.localidade
+
     }
+     
 }
-let botao = document.querySelector("#botao")
+let botao = document.querySelector("#botao");
 botao.addEventListener("click", ConsultaCep);
