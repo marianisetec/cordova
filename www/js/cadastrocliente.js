06cee928f8697b57e32ed6126defb38507afe62d
mariana <marianabodario>
2024-05-03 22:40:32 -0300

    lançar dados na API

--- www/js/cadastrocliente.js
+++ www/js/cadastrocliente.js
@@ -2,24 +2,23 @@ function enviar(){
 
     const dbParam = JSON.stringify(
         {
-            "Nome": document.querySelector("#nome").value,
-            "Documento": document.querySelector("#documento").value,
-            "Celular": document.querySelector("#celular").value,
-            "Numero": document.querySelector("#numero").value,
-           "Endereco": document.querySelector("#cep").value,
-           "Bairro": document.querySelector("#bairro").value,
-           "Cidade": document.querySelector("#cidade").value,
-           "Estado": document.querySelector("#estado").value,
+            "nome": document.querySelector("#nome").value,
+            "documento": document.querySelector("#documento").value,
+            "celular": document.querySelector("#celular").value,
+            "numero": document.querySelector("#numero").value,
+           "endereco": document.querySelector("#cep").value,
+           "bairro": document.querySelector("#bairro").value,
+           "cidade": document.querySelector("#cidade").value,
+           "estado": document.querySelector("#estado").value,
         }
     );
     const xmlhttp = new XMLHttpRequest();
     xmlhttp.onload = function() {
-        const myObj = JSON.parse(this.responseText);
-        console.log(myObj);
-        document.getElementById("demo").innerHTML = myObj;
+            console.log(xmlhttp.response)
     }
     xmlhttp.open("POST", "https://etec.fernandograciano.com.br/cadastrocliente.php");
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    console.log(dbParam)
     xmlhttp.send("dados="+dbParam );
 
 }
---
+++ www/js/lerclientes.js
@@ -0,0 +1,27 @@
+function lerclientes()
+{
+    let requisicao = new XMLHttpRequest();
+    let url = "https://etec.fernandograciano.com.br/listarclientes.php";
+    requisicao.open("GET", url);
+    requisicao.send();
+    requisicao.onload = function()
+    {
+
+        let corpotabela = document.querySelector("#corpotabela")
+        let dados = JSON.parse(this.response)
+        dados.forEach(function (valor,chave)
+        {
+
+            let novalinha = document.createElement("tr")
+            let celulaNome = document.createElement("td")
+            let celularCelular =  document.createElement("td")
+            celulaNome.innerHTML = valor.nome
+            celularCelular.innerHTML = valor.celularCelular
+            novalinha.append(celulaNome)
+            novalinha.append(celulaCelular)
+            corpotabela.append(novalinha)
+        });
+
+        console.log(this.response)
+    }
+}
